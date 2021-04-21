c864f2b03d6e0ee70588f518d5f63da0785a6576
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-04-21 20:23:34 -0300

    .gitignore: Adicionado vscode settings.

--- src/server.ts
+++ src/server.ts
@@ -6,19 +6,4 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-/**
- * HTTP VERBS:
- * GET = Retrieve Data
- * POST = Create Data
- * PUT = Alter Data
- * DELETE = Delete Data
- * PATCH = Alter Specific Data
- */
-
-app.get("/", (request, response) =>{
-    return response.json({
-        message: "Olá NLW#5!"
-    });
-});
-
 app.listen(1000, () => console.log("Server running against http://localhost:1000"));
